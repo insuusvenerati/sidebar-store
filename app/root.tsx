c9dff08d41271351b7a61221e22d07da15f64712
Sean Norwood <norwood.sean@gmail.com>
2022-10-27 14:29:06 -0500

    Add OG

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,5 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import tailwind from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
@@ -27,6 +20,28 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta name="generator" content="Big Cartel" />
+        <meta name="author" content="↟CAMP MUSTELID↟"></meta>
+        <meta
+          name="description"
+          content="we're a small team of two artists who love nature! @nepeteaa and @melmorrel you can visit our website here: campmustelid.com"
+        ></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:site_name" content="↟CAMP MUSTELID↟"></meta>
+        <meta property="og:title" content="↟CAMP MUSTELID↟"></meta>
+        <meta property="og:url" content="https://www.campmustelid.shop/"></meta>
+        <meta
+          property="og:description"
+          content="we're a small team of two artists who love nature! @nepeteaa and @melmorrel you can visit our website here: campmustelid.com"
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://assets.bigcartel.com/account_images/3478508/IMG_4380.PNG?auto=format&amp;fit=max&amp;h=1200&amp;w=1200"
+        ></meta>
+        <meta
+          property="og:image:secure_url"
+          content="https://assets.bigcartel.com/account_images/3478508/IMG_4380.PNG?auto=format&amp;fit=max&amp;h=1200&amp;w=1200"
+        ></meta>
         <Meta />
         <Links />
       </head>
